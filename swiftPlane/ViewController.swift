02a35eee663adf85c8e77064426599080f0c14f4
_____张智华______ <Zhang_ZhiHua2@want-want.com>
2019-01-29 19:44:29 +0800

    zzh _____________ **整理代码 添加可控量** ______________

--- swiftPlane/ViewController.swift
+++ swiftPlane/ViewController.swift
@@ -12,20 +12,37 @@ import CoreFoundation
 let planeWidth = CGFloat(33.0)
 let planeHeight = CGFloat(40.0)
 
-let dijiWidth = CGFloat(30.0)
-let dijiHeight = CGFloat(37.0)
+let dijiWidth = CGFloat(37.0)
+let dijiHeight = CGFloat(30.0)
+
+let kScreenHeight = CGFloat(UIScreen.main.bounds.size.height)
+let kScreenWidth = CGFloat(UIScreen.main.bounds.size.width)
 
 
 
 class ViewController: UIViewController {
+    
+    var Hertz:Double = 30.0 //刷屏速度
+    
+    var centerButton:UIButton!
     
     var bgview1: UIImageView!
     var bgview2: UIImageView!
     var planeView: UIImageView!
     var zidanArray: NSMutableArray!
     var dijiArray: NSMutableArray!
+    var scoreLabel: UILabel!
     var count = 0
+    var score = 0
     var timer:Timer!
+    
+    var activeZiDanNum:Int = 3 //控制子弹的发射密集程度
+    var activeDiJiNum:Int = 10 //控制第几的下落速度
+    var DiJiSpace:CGFloat = 5.0 //控制第几的速度
+    var ZiDanSpace:CGFloat = 30.0 //控制子弹的速度
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -34,7 +51,10 @@ class ViewController: UIViewController {
         initView()
         createZiDan()
         createDiJi()
-        self.starMethod()
+        //缓冲
+        createBoomView(CGRect.zero)
+        
+        
     }
     
     func initView(){
@@ -59,12 +79,36 @@ class ViewController: UIViewController {
         self.planeView.animationImages=imgArray
         self.planeView.startAnimating()
         
-        
         self.planeView.frame = CGRect(x:self.view.frame.width/2 - 20 , y:self.view.frame.height - 60 , width:planeWidth , height:planeHeight)
         //        self.planeView.backgroundColor = .red
         
         self.view.addSubview(self.planeView)
+        
+        self.centerButton = UIButton()
+        self.centerButton.setTitle("开始游戏", for: .normal)
+        self.centerButton.setTitleColor(.red, for: .normal)
+        self.centerButton.addTarget(self, action: #selector(star), for: .touchUpInside)
+        self.centerButton.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        self.centerButton.bounds = CGRect(x:0,y:0,width:80,height:20)
+        self.view.addSubview(self.centerButton)
+        
+        
+        self.scoreLabel = UILabel()
+        self.scoreLabel.frame = CGRect(x:kScreenWidth - 100,y:kScreenHeight-30,width:80,height:20)
+        self.scoreLabel.text="0"
+        self.scoreLabel.textAlignment = NSTextAlignment.right
+        self.scoreLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight(rawValue: 2))
+        self.view.addSubview(self.scoreLabel)
+        
+        
+        
     }
+    
+    @objc func star(){
+        self.starMethod()
+        self.centerButton.alpha = 0
+    }
+    
     
     func createZiDan(){
         self.zidanArray = NSMutableArray()
@@ -125,7 +169,7 @@ class ViewController: UIViewController {
         for zidan in self.zidanArray{
             if((zidan as! UIImageView).tag==5){
                 var temp = (zidan as! UIImageView).frame
-                temp.origin.y -= 30
+                temp.origin.y -= self.ZiDanSpace
                 (zidan as! UIImageView).frame = temp
                 if(temp.origin.y < -30){
                     (zidan as! UIImageView).tag=6
@@ -138,7 +182,7 @@ class ViewController: UIViewController {
         for diji in self.dijiArray {
             if((diji as! UIImageView).tag==5){
                 var temp = (diji as! UIImageView).frame
-                temp.origin.y += 5
+                temp.origin.y += self.DiJiSpace
                 (diji as! UIImageView).frame = temp
                 if(temp.origin.y > self.view.frame.height){
                     (diji as! UIImageView).tag=6
@@ -160,6 +204,8 @@ class ViewController: UIViewController {
                             createBoomView((diji as! UIImageView).frame)
                             (zidan as! UIImageView).frame = CGRect.zero
                             (diji as! UIImageView).frame = CGRect.zero
+                            score += 10
+                            self.scoreLabel.text = String(format:"%d", score)
                         }
                     }
                 }
@@ -187,7 +233,9 @@ class ViewController: UIViewController {
     
     
     func starMethod(){
-        self.timer =  Timer.scheduledTimer(timeInterval: 0.033, target: self, selector: #selector(update), userInfo: "parameter", repeats: true)
+//        CGFloat Hertz = CGFloat(1.0/self.Hertz)
+        
+        self.timer =  Timer.scheduledTimer(timeInterval:1.0/self.Hertz, target: self, selector: #selector(update), userInfo: "parameter", repeats: true)
         
         //        RunLoop.current.add(Timer:timer, forMode: RunLoop.Mode)
         //        let loop = RunLoop.current
@@ -210,11 +258,11 @@ class ViewController: UIViewController {
         
         self.count = self.count+1
         
-        if count%3==0{
+        if count%self.activeZiDanNum==0{
             jihuo()
             
         }
-        if count%10==0{
+        if count%self.activeDiJiNum==0{
             jihuoDiji()
         }
         
